8da66200c77c22198e4da98c0118b9db5a3d8b69
Luca De Vitis <luca.devitis@moneysupermarket.com>
2016-04-12 09:11:31 +0100

    Turned symbols into string, more code cleanup

--- lib/rpm.rb
+++ lib/rpm.rb
@@ -1,78 +1,91 @@
+require 'process'
+require 'pty'
+
 module RPM
-  def rpmbuild (target, options)
-    command = options[:rpmbuild] || 'rpmbuild'
-    stage = '-' << case
-      when file = options[:tar] then 't'
-      when file = options[:spec] then 'b'
-      else raise ArgumentError, 'Missing either spec or tar file option'
-    end << case target
-      when :all then 'a'
-      when :binary then 'b'
-      when :source then 's'
-      when :prep then 'p'
-      when :build then 'c'
-      when :install then 'i'
-      when :list, :check, :list_check then 'l'
-      else raise ArgumentError, "Unknown building stage `#{target}'"
-    end
-    args = [command, stage] << options.map do |opt, value|
-      case opt
-        when :debug then '-vv'
-        when :verbose then '-v'
-        when :clean, :nobuild, :rmsource, :rmspec, :short_circuit, :quiet
-          "--#{opt.to_s.tr('_', '-')}"
-        when :target, :buildroot, :root, :dbpath, :rcfile
-          "--#{opt} #{value}"
-        else raise ArgumentError, "Unknown option `#{opt}'"
-      end if value
-    end
-    output = %x(#{args.join(' ')})
-    [$?, output]
+  def command(*argv)
+    input, output = [nil, nil]
+    argv ||= yield if block_given?
+    input, output, pid = PTY.spawn(*argv)
+    Process::wait(pid)
+    [$?, input.read]
+  ensure
+    input.close if input
+    output.close if output
   end
 
-  def mock (action, options)
-    config = options.map {|name, value| [name.to_sym.tr('_', '-'), value]}.to_h
-    command = confg[:mock] || 'mock'
-    args = [command, "--#{action}"] + case action.to_sym
-    when :rebuild
-      raise ArgumentError, 'No SRPM specified' unless config[:srpm]
-      [config.delete(:srpm)]
-    when :buildsrpm
-      if config[:spec]
-        ['--spec', config.delete(:spec)]
-      elsif config[:sources]
-        ['--sources', config.delete(:sources)]
-      elsif config[:'scm-enable']
-        config.delete(:'scm-enable')
-        [
-          '--scm-enable',
-          config.delete(:'scm-option').map {|k, v| "--scm-option #{k}=#{v}"}
-        ]
-      else
-        raise ArgumentError, 'No spec, sources or scm specified'
+  def rpmbuild (target, config)
+    command do
+      options = config.map {|name, value| [name.to_s.tr('_', '-'), value]}.to_h
+      stage = '-' << case
+      when file = options.delete('tar') then 't'
+      when file = options.delete('spec') then 'b'
+      else raise ArgumentError, 'Missing spec or tar file'
       end
-    when :install, :remove
-      raise ArgumentError, 'No package specifies' unless config[:package]
-      [config.delete(:package)]
-    when :copyin, :copyout
-      raise ArgumentError, 'No path specified' unless (config[:path] || []).size < 2
-      [config.delete(:path)]
-    when :update, :orphanskill then []
-    else
-      raise ArgumentError, "Unknown action `#{action}'"
+      stage << case target.to_s
+      when 'all' then 'a'
+      when 'binary' then 'b'
+      when 'source' then 's'
+      when 'prep' then 'p'
+      when 'build' then 'c'
+      when 'install' then 'i'
+      when 'list', 'check', 'list-check' then 'l'
+      else raise ArgumentError, "Unknown building stage: #{target}"
+      end
+      argv = [options.delete('rpmbuild') || 'rpmbuild', stage]
+      argv += options.map do |name, value|
+        case name
+          when 'debug' then ['-vv']
+          when 'verbose' then ['-v']
+          when 'clean', 'quiet', 'nobuild', 'short-circuit',
+               'rmsource', 'rmspec'
+            ["--#{name}"]
+          when 'target', 'buildroot', 'root', 'dbpath', 'rcfile'
+            ["--#{name}", "#{value}"]
+          else raise ArgumentError, "Unknown option #{name}"
+        end if value
+      end.flatten
+      argv += [file].flatten
     end
-    args += [
-             :offilne, :'no-clean', :'cleanup-after', :'no-cleanup-after',
-             :quiet, :verbose, :unpriv, :trace
-    ].select {|name| options[name]}.map {|name| "--#{name}"}
-    args += [
-             :root, :target, :arch, :resultdir,
-             :uniqueext, :configdir, :rpmbuild_timeout, :cwd,
-    ].select {|name| options[name]}.map {|name| "--#{name}=#{options[name]}"}
---define="MACRO EXPR"
---with=OPTION
---without=OPTION
---enable-plugin=PLUGIN
---disable-plugin=PLUGIN
+  end
+
+  def mock (action, config)
+    command do
+      options = config.map {|name, value| [name.to_s.tr('_', '-'), value]}.to_h
+      argv = []
+      argv += ['sudo'] + options.delete('sudo').split + ['--'] if options['sudo']
+      argv += [options.delete('mock') || 'mock', "--#{action}"]
+      argv += case action.to_s
+      when 'rebuild'
+        [options.delete('srpm')]
+      when 'buildsrpm'
+        case
+        when options['spec'] then ['--spec', options.delete('spec')]
+        when options['sources'] then ['--sources', options.delete('sources')]
+        when options['scm-enable']
+          options.delete('scm-enable') && ['--scm-enable']
+        end
+      when 'chroot'
+        [options.delete('command')]
+      when 'install', 'remove'
+        [options.delete('package')]
+      when 'installdeps'
+        case
+        when options['srpm'] then [options.delete('srpm')]
+        when options['rpm'] then [options.delete('rpm')]
+        end
+      when 'copyin', 'copyout'
+        options.delete('path')
+      when 'init', 'clean', 'update', 'orphanskill' then []
+      else
+        raise ArgumentError, "Unknown action `#{action}'"
+      end
+      argv += options.map do |name, value|
+        case value
+        when String then ["--#{name}", value]
+        when Array then value.map {|v| ["--#{name}", v]}
+        when Hash then value.map {|k, v| ["--#{name}", "#{k}=#{v}"]}
+        end
+      end.flatten
+    end
   end
 end
